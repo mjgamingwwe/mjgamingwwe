908aa82751fd64afdddab80b72d7fbd1dbce7dac
mjgamingwwe <109023508+mjgamingwwe@users.noreply.github.com>
2023-09-02 17:04:24 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+
+# Your code for initializing the bot, handling commands, and processing messages
+
+if __name__ == "__main__":
+    main()
